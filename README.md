a4b7035dd6d3b2f9d9ed086bac2a43d4f1c1b76e
avetruvio <avetruvioav@gmail.com>
2023-08-09 13:18:14 -0700

    debugging step 1 and narrowing stuff down

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Linux box setup
     This is a standardized install for the vms and machines for a fast setup of the systems with basic essentials, including hosting for certian services and setup for machines for just getting things going fast
     
+
+
+To Use
+wget https://raw.githubusercontent.com/avetruvio/setup/main/basicsetup.sh && chmod +x && ./basicsetup.sh
--- basicsetup.sh
+++ basicsetup.sh
@@ -15,39 +15,52 @@ fi
 echo "number 1-4 based on the type of install"
 echo "1-5 1 is headless server, 2 is vpn headless server, 3 is netbook, 4 is desktop, 5 is vps"
 read -r -n 1 var1 
-
+echo "install tailscale y or n"
+read -r -n 1 var2
 echo "begenning setup standby" 
 #inistal system stup, bisic tools and full update
 apt -y update
 apt -y upgrade
 # install utils
-apt -y install openssh-server tmux wget git
+apt -y install openssh-server tmux wget git curl software-properties-common
 #ssh pubkey pull
 echo pulling ssh keys
-wget 
+#wget 
 #this is where it dynamicizes from the basic install
 if [ "$var1" == 1 ]  
 #headless server
 then 
-    apt install easyrsa arp-scan
+    apt install -y easyrsa arp-scan
 elif [ "$var1" == 2 ]
-#
 then 
-    apt install openvpnheadless server easyrsa arp-scan
+    apt install -y openvpnheadless server easyrsa arp-scan
 elif [ "$var1" == 3 ]
 #netbook
 then 
-    apt install i3 i3lock lightdm cool-retro-term compton i3-sensible-terminal xscreensaver xscreensaver-data xscreensaver-data-extra
+    add-apt-repository ppa:vantuz/cool-retro-term
+    apt update
+    apt install -y i3 i3lock lightdm cool-retro-term compton xscreensaver xscreensaver-data xscreensaver-data-extra
 elif [ "$var1" == 4 ]
 #desktop
 then 
-    apt install i3 i3lock lightdm cool-retro-term compton  i3-sensible-terminal xscreensaver xscreensaver-data xscreensaver-data-extra 
-elif ["$var1" == 5 ]
+    add-apt-repository ppa:vantuz/cool-retro-term
+    apt update
+    apt install -y i3 i3lock lightdm cool-retro-term compton xscreensaver xscreensaver-data xscreensaver-data-extra 
+elif [ "$var1" == 5 ]
 #vps 
 then 
     apt install openvpn 
 fi
-
+#install tailscale segment 
+if [ "$var2" == y ]
+then
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+apt update
+apt install tailscale
+else
+echo tailscale install bypassed, continuing 
+fi
 #install configs from github
 #echo "installing config"
 
@@ -57,3 +70,8 @@ fi
 #dotfiles config --local status.showUntrackedFiles no
 
 #needs to be finished later, gonna be a pain to do 
+
+
+
+#remove any excess packages
+apt autoremove -y 
