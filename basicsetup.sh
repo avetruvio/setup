206f8dc382c6d2a51b4369435284fa6df6acb7a9
avetruvio <avetruvioav@gmail.com>
2023-08-09 23:02:31 -0700

    tidying up spacing in variable select

--- basicsetup.sh
+++ basicsetup.sh
@@ -14,11 +14,11 @@ fi
 #define variable 
 echo "number 1-4 based on the type of install"
 echo "1-5 1 is headless server, 2 is vpn headless server, 3 is netbook, 4 is desktop, 5 is vps"
-echo "#"
-read -r -n 1 var1 
+read -r -n 1 var1
+echo " " 
 echo "install tailscale y or n"
-echo " "
 read -r -n 1 var2
+echo " "
 echo "begenning setup standby" 
 #inistal system stup, bisic tools and full update
 apt -y update
